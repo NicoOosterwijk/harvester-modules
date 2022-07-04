50db242e4202826c7a1f05aa17fab215f96d43ff
Nico Oosterwijk <nico@digitalinfo.nl>
2022-07-04 15:18:58 +0200

    Multi-disk config

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -1,7 +1,53 @@
-resource "harvester_virtualmachine" "vm" {
-  name      = var.name
+resource "harvester_virtualmachine" "ubuntu20" {
+  count     = var.number_of_hosts
+  name      = "${var.prefix}-${count.index +1}"
   namespace = var.namespace
 
-  disk              = var.disk
-  network_interface = var.nic
+  tags = {
+    ssh-user = var.ssh_user
+  }
+
+  network_interface {
+    name         = var.nic_name
+    network_name = var.nic_network
+  }
+
+  ## Boot disk
+  dynamic "disk" {
+    for_each = [for disk in var.disk1 : disk]
+    content {
+      name        = disk.value.name
+      type        = disk.value.type
+      size        = disk.value.size
+      bus         = disk.value.bus
+      boot_order  = disk.value.boot_order
+      image       = disk.value.image
+      auto_delete = disk.value.auto_delete
+    }
+  }
+
+  ## additional disks
+  dynamic "disk" {
+    for_each = [for disk in var.extradisks : disk]
+    content {
+      name        = disk.value.name
+      type        = disk.value.type
+      size        = disk.value.size
+      bus         = disk.value.bus
+      boot_order  = disk.value.boot_order
+      image       = disk.value.image
+      auto_delete = disk.value.auto_delete
+    }
+  }
+
+  cpu          = var.cpu
+  memory       = var.memory
+  run_strategy = var.run_strategy
+  hostname     = "${var.prefix}-${count.index +1}"
+  machine_type = var.machine_type
+
+  cloudinit {
+    user_data    = var.user_data
+    network_data = var.network_data
+  }
 }
--- virtualmachine/variables.tf
+++ virtualmachine/variables.tf
@@ -1,6 +1,17 @@
-variable "name" {}
-variable "namespace" {}
 variable "kubeconfig" {}
-
-variable "disk" {}
-variable "nic" {}
+variable "namespace" {}
+variable "number_of_hosts" {}
+variable "prefix" {}
+variable "cpu" {}
+variable "memory" {}
+variable "run_strategy" {}
+//variable "hostname" {}
+variable "machine_type" {}
+variable "nic_name" {}
+variable "nic_network" {}
+variable "ssh_public_key" {}
+variable "ssh_user" {}
+variable "disk1" {}
+variable "extradisks" {}
+variable "user_data" {}
+variable "network_data" {}

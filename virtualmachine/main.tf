4c828810b38645351414c0109e6e6aba3d74ed54
Nico Oosterwijk <nico@digitalinfo.nl>
2022-07-05 11:15:37 +0200

    Add output

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -57,5 +57,5 @@ output "vm_name" {
   value = "${harvester_virtualmachine.ubuntu20[0].name}"
 }
 output "vm_ip" {
-  value = "${harvester_virtualmachine.ubuntu20[0].network_data}"
+  value = "${harvester_virtualmachine.ubuntu20[0].cloudinit}"
 }

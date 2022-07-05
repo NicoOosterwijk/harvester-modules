653c2724a05f094015cfbbdd16f36d018c111be1
Nico Oosterwijk <nico@digitalinfo.nl>
2022-07-05 11:27:53 +0200

    Add output

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -54,8 +54,8 @@ resource "harvester_virtualmachine" "ubuntu20" {
 
 
 output "vm_name" {
-  value = "${harvester_virtualmachine.ubuntu20[*].name}"
+  value = "${harvester_virtualmachine.ubuntu20[0].name}"
 }
 output "vm_ip" {
-  value = "${harvester_virtualmachine.ubuntu20[*].network_interface[*].ip_address}"
+  value = "${harvester_virtualmachine.ubuntu20[0].network_interface[*].ip_address}"
 }

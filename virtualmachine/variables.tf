1add27848f92ab3974c8ebe4164e43dc57fff1be
Nico Oosterwijk <nico@digitalinfo.nl>
2022-07-06 10:17:36 +0200

    Add nodes config

--- virtualmachine/variables.tf
+++ virtualmachine/variables.tf
@@ -15,3 +15,18 @@ variable "disk1" {}
 variable "extradisks" {}
 variable "user_data" {}
 variable "network_data" {}
+
+variable "nodes" {
+    description = "Cluster Nodes configuration"
+    type = map(object({
+        num_cpus      = number
+        memory        = number
+        roles         = list(string)
+        template_name = string
+        os_disk_size  = number
+        num_nodes     = optional(number)
+        enable_longhorn_storage              = optional(bool)
+        enable_longhorn_exclusive_scheduling = optional(bool)
+        enable_longhorn_data_locality        = optional(bool)
+    }))
+}
